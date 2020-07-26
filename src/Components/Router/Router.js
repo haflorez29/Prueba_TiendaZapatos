94913ced719373b6ed1ea4f87e970d34deeed9dc
haflorez29 <heidyflorez55@gmail.com>
2020-07-25 22:12:17 -0500

    inicializacion

---
+++ src/Components/Router/Router.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import App from '../../Pages/App'
+
+const Router = () =>(
+  <BrowserRouter>
+  <Switch>
+    <Route exact path='/' component={App}></Route>
+    </Switch>  
+  </BrowserRouter>
+)
+
+export default Router
