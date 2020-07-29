58c068a5d5733417f0fce35c86c893b9a1b2e63b
haflorez29 <heidyflorez55@gmail.com>
2020-07-28 23:30:50 -0500

    nav

--- src/Components/Resultados/Filtro.js
+++ src/Components/Resultados/Filtro.js
@@ -1,23 +1,24 @@
 import React from 'react'
+import "../../Styles/Filtro.css"
 
 const Filtro = ({title,o1,o2,o3,o4,o5, style}) =>{
   return(
     <form className={style}>
-        <legend>{title}</legend>
+        <legend className="poppins18">{title}</legend>
         <div className='d-flex flex-column'>
-        <label>
-            <input type="radio" name={title}  value={o1}/> {o1}
+        <label className="tinos14">
+            <input type="radio" name={title}  value={o1} className="radiobutton" id="radiobuton"/> {o1}
         </label>
-        <label>
+        <label className="tinos14">
             <input type="radio" name={title} value={o2}/> {o2}
         </label>
-        <label>
+        <label className="tinos14">
             <input type="radio" name={title} value={o3}/> {o3}
         </label>
-        <label>
+        <label className="tinos14">
             <input type="radio" name={title} value={o4}/> {o4}
         </label>
-        <label>
+        <label className="tinos14">
             <input type="radio" name={title} value={o5}/> {o5}
         </label>
         </div>
