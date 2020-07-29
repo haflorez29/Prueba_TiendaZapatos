7aaa7a94afea91d74586b7dd3efc78f2efb21187
AngieEslava <egangiest@gmail.com>
2020-07-29 17:35:00 -0500

    vista detalle de producto

--- src/Components/Router/Router.js
+++ src/Components/Router/Router.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from '../../Pages/App'
 import Resultados from '../Resultados/Resultados'
+import DetalleProducto from '../DetalleProducto/DetalleProducto'
 
 const Router = () =>(
   <BrowserRouter>
   <Switch>
     <Route exact path='/' component={App}></Route>
     <Route exact path='/Resultados' component={Resultados}></Route>
+    <Route exact path='/DetalleProducto' component={DetalleProducto}></Route>
     </Switch>  
   </BrowserRouter>
 )
