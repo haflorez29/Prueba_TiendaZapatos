792dbb1d155cde39b1feec7ad1c3e1d418eae50a
eri970301 <erika970301@live.com>
2020-07-29 20:30:44 -0500

    carritos y compra

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../Styles/App.css';
+import Footer from '../Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <p>home</p>
+      <Footer></Footer>
     </div>
   );
 }
