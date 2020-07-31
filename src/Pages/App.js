21e5d29c2aee4a7871b82843fac3725af3f5b292
haflorez29 <heidyflorez55@gmail.com>
2020-07-30 20:04:02 -0500

    consumo apifake

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Nav from '../Components/Share/Nav'
 import Carusel from '../Components/Home/Carusel'
+import axios from 'axios'
 import '../Styles/App.css';
 
 function App() {
+  const [zapatos, setZapatos] = useState([{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}])
+
+  useEffect(() => {
+    const getUrl = "https://api-jsonserver-now.vercel.app/db.json";
+    axios.get(getUrl).then((res) => {
+      // console.log(res.data.zapatos)
+      const data = res.data.zapatos;
+      if (data !== null) {
+        setZapatos(data        );
+      }else{
+        console.log("");
+      }
+    });
+  },[]);
+
+  console.log(zapatos[2]) 
+
   return (
     <div className="App container-fluid">
       <Nav></Nav>
       <Carusel></Carusel>
       <div>
-        <h3 className="poppins32">Artículos Recomendados</h3>
-        <img src="..." alt="name"></img>
+      <h3 className="poppins32">Artículos Recomendados</h3>
+        {zapatos.map((item) =>{
+          return(
+            <React.Fragment>
+            <img src={item.imagen}alt="name"></img>
+            </React.Fragment>
+          )
+        })}
+      
         <h3 className="poppins32">Otros Usuarios Compraron</h3>
         <img src="..." alt="name"></img>
         </div>       
